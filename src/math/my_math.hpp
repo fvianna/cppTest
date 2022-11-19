6ef7da987c393fd67a2557a5264e603d9ca2cb21
Francisco Vianna <francisco.v.vianna@gmail.com>
2022-11-18 22:00:42 -0300

    - Adiciona e faz uso de uma simples biblioteca de recursos matemáticos - Build feito pelo CMake

---
+++ src/math/my_math.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace util::math
+{
+    // a hack square root calculation using simple operations
+    double mySqrt(double x);
+}
---
+++ src/math/my_sqrt.cpp
@@ -0,0 +1,25 @@
+namespace util::math
+{
+
+// a hack square root calculation using simple operations
+double mySqrt(double x)
+{
+  if (x <= 0) {
+    return 0;
+  }
+
+  double result = x;
+
+  // do ten iterations
+  for (int i = 0; i < 10; ++i) {
+    if (result <= 0) {
+      result = 0.1;
+    }
+    double delta = x - (result * result);
+    result = result + 0.5 * delta / result;
+  }
+
+  return result;
+}
+
+}
